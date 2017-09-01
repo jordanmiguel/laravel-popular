1b7356ddd14d34bb105c8d205b280e5172da2792
Jordan Miguel <email@jordanmiguel.com>
2017-09-01 05:22:20 -0300

    Package Structure

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All Notable changes to Laravel Popular will be documented in this file.
+
+Updates should follow the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
+
+### Added
+- Nothing
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
---
+++ src/LaravelPopularServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JordanMiguel\LaravelPopular;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPopularServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
