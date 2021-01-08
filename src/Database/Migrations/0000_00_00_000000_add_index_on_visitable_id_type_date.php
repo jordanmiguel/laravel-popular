cf371159b1178ae35b5abb6e80878e709c1b63ba
Shawn Mayzes <shawn@codelabs.ca>
2021-01-07 22:46:44 -0800

    Adding a performance boost with an index on visitable_id, visitable_type, and date.

---
+++ src/Database/Migrations/0000_00_00_000000_add_index_on_visitable_id_type_date.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexOnVisitableIdTypeDate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->index(['visitable_id', 'visitable_type', 'date']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->dropIndex(['visitable_id', 'visitable_type', 'date']);
+        });
+    }
+}
